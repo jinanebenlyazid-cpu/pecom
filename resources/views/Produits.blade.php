cc8ca04ac8ec7b867f4ba87ff322c7ea700f8178
Jinane <jinanebenliazid@gmail.com>
2026-01-08 18:01:42 +0100

    insp4

--- resources/views/Produits.blade.php
+++ resources/views/Produits.blade.php
@@ -21,7 +21,7 @@
                     <td class="fw-semibold">{{ $item['nom'] }}</td>
                     <td>{{ $item['prix'] }} DH.</td>
                     <td>
-                        <img src="{{ asset('images' . $item['image']) }}" alt="{{ $item['nom'] }}" class="img-thumbnail" style="width: 90px; height: 90px; object-fit: cover;">
+                        <img src="{{ asset('images/' . $item['image']) }}" alt="{{ $item['nom'] }}" class="img-thumbnail" style="width: 90px; height: 90px; object-fit: cover;">
                     {{ asset('images' . $item['image']) }}
                     </td>
                 </tr>
